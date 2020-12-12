fd5bca9d74faf8edb85a5d5b2836ac5abd913e50
Doc1998 <docmp98@gmail.com>
2020-12-12 14:47:56 +0000

    Cors fix

--- src/main/java/CS4125/group5/config/WebConfig.java
+++ src/main/java/CS4125/group5/config/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/auth/signup")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("*")
                 .maxAge(3600L)
